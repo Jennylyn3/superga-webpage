218dfbbb5b20d35d2b6b10cc7052cf3ef3f04545
Jennylyn3 <@jhenlarcena@gmail.com>
2020-05-29 16:06:36 +0800

    second commit

---
+++ gallery.js
@@ -0,0 +1,71 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+    products: [
+        {
+            "imgSrc": "assets/gallery1.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery2.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery3.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery4.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery5.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery6.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery7.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery8.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery9.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery1.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery10.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery11.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery12.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery13.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery14.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery15.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery16.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery17.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery18.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery19.jpg"
+        },
+        {
+            "imgSrc": "assets/gallery20.jpg"
+        },
+        
+    ]
+}
+})
